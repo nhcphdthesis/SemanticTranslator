673fabaa56c71c57a6020e087a44817e8b929d7c
nhcphdthesis <niehongchao1983@163.com>
2013-10-30 13:44:33 +0800

    add lib

--- src/main/java/org/mule/transport/cutome/transformers/SemanticTranslator.java
+++ src/main/java/org/mule/transport/cutome/transformers/SemanticTranslator.java
@@ -3,13 +3,17 @@ package org.mule.transport.cutome.transformers;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.mule.api.MuleMessage;
 import org.mule.api.annotations.ContainsTransformerMethods;
 import org.mule.api.annotations.Transformer;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageTransformer;
+import org.mule.transformer.types.DataTypeFactory;
 
 @ContainsTransformerMethods 
-public class SemanticTranslator {
+public class SemanticTranslator extends AbstractMessageTransformer{
 	public SemanticTranslator() {
-        registerSourceType(DataTypeFactory.create(ObjectToStringTransformer.class));
+        registerSourceType(DataTypeFactory.STRING);
         setReturnDataType(DataTypeFactory.STRING);
         setName("SemanticTranslator");
     }
@@ -18,4 +22,10 @@ public class SemanticTranslator {
 	  {
 	      return new java.net.URL(string);
 	  }
+	@Override
+	public Object transformMessage(MuleMessage arg0, String arg1)
+			throws TransformerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
