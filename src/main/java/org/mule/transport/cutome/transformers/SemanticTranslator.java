c4761ae5a58b2bd592f5971d8c3d7f0891ac0585
nhcphdthesis <niehongchao1983@163.com>
2013-10-30 20:09:13 +0800

    implement

--- src/main/java/org/mule/transport/cutome/transformers/SemanticTranslator.java
+++ src/main/java/org/mule/transport/cutome/transformers/SemanticTranslator.java
@@ -1,31 +1,59 @@
 package org.mule.transport.cutome.transformers;
-
+//author: NHC
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URL;
 
-import org.mule.api.MuleMessage;
+import org.mindswap.pellet.jena.PelletReasonerFactory;
 import org.mule.api.annotations.ContainsTransformerMethods;
 import org.mule.api.annotations.Transformer;
-import org.mule.api.transformer.TransformerException;
-import org.mule.transformer.AbstractMessageTransformer;
-import org.mule.transformer.types.DataTypeFactory;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 @ContainsTransformerMethods 
-public class SemanticTranslator extends AbstractMessageTransformer{
-	public SemanticTranslator() {
-        registerSourceType(DataTypeFactory.STRING);
-        setReturnDataType(DataTypeFactory.STRING);
-        setName("SemanticTranslator");
-    }
+public class SemanticTranslator{
 	@Transformer
-	  public String translate(String string) throws MalformedURLException
+	  public String translate(String s) throws MalformedURLException
 	  {
-	      return "";
+		//1. parse input
+		OntModel input = ModelFactory.createOntologyModel();
+		input.read(s,null);
+		
+		String mapping_URL = "";
+		String queryString = "";
+		InputStream in_mapping;
+		
+		try {
+			//2. retrieve mapping
+			in_mapping = new FileInputStream(new File(mapping_URL));
+			input.add(ModelFactory.createDefaultModel().read(in_mapping,null));
+			in_mapping.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//3. reasoning
+		OntModel inferred = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC, input);
+		//4. serialization result
+		Query query = QueryFactory.create(queryString);
+		// Execute the query and obtain results
+		QueryExecution qe = QueryExecutionFactory.create(query, inferred);
+		ResultSet results = qe.execSelect();
+		// Output query results	
+		ResultSetFormatter.asXMLString(results);
+	    return inferred.toString();
 	  }
-	@Override
-	public Object transformMessage(MuleMessage arg0, String arg1)
-			throws TransformerException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
